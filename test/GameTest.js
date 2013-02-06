3ab7cb4df550650b00508b9939b16b73bce3a23a
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-02-06 17:30:36 +0100

    test: drone

--- test/GameTest.js
+++ test/GameTest.js
@@ -35,4 +35,23 @@ describe ("When the game has not started", function () {
         expect(devoxx).toHaveCss({position:'fixed', opacity:'0'});
         expect(devoxx).toHaveAttr("src","img/bg.jpg");
     });
+
+    it ("the ship is simply over the earth and waiting for start",function () {
+
+        setFixtures(sandbox());
+
+        var s = "#sandbox";
+
+        start($(s));
+        expect($(s)).toExist();
+        expect($(s)).toHaveCss({margin:"0px"});
+
+        var drone = $(s).find(".drone");
+        expect(drone).toExist();
+        expect(drone).toBe("img");
+        expect(drone).toHaveCss({position:'fixed'});
+        expect(drone).toHaveCss({top:'380px'});
+        expect(drone).toHaveCss({left:'120px'});
+        expect(drone).toHaveAttr("src","img/heli1.png");
+    });
 });
