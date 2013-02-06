d5886024d2d834e2fd1d7c352a43def09839d134
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-02-06 17:29:42 +0100

    test: earth

--- test/GameTest.js
+++ test/GameTest.js
@@ -1,2 +1,20 @@
 describe ("When the game has not started", function () {
+    it ("the earth is displayed", function () {
+
+        setFixtures(sandbox());
+
+        var s = "#sandbox";
+
+        start($(s));
+        expect($(s)).toExist();
+        expect($(s)).toHaveCss({margin:"0px"});
+
+        var earth = $(s).find(".earth");
+        expect(earth).toExist();
+        expect(earth).toBe("img");
+        expect(earth).toBeVisible();
+        expect(earth).toHaveCss({position:'fixed'});
+        expect(earth).toHaveAttr("src","img/earth.png");
+
+    });
 });
