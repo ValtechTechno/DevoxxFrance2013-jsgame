02b6f46aba7421b8a07501553327da2430b5a9b7
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-02-06 17:39:03 +0100

    add: drone

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 function start(root) {
     root.css({margin:0});
-    root.append("<img class='earth'> ").append("<img class='devoxx'>");
+    root.append("<img class='earth'> ").append("<img class='devoxx'>").append("<img class='drone'>");
     root.find(".earth").css({position:"fixed", height:"100%"}).attr("src","img/earth.png");
     root.find(".devoxx").css({position:"fixed", height:"100%",opacity:0}).attr("src","img/bg.jpg");
+    root.find(".drone").css({position:"fixed", top: 380, left: 120, zIndex:3}).attr("src","img/heli1.png");
 }
