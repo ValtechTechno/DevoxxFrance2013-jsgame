fd73bafa25c0cc0e8f337c87ec33acad0a6b6859
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-02-06 17:41:13 +0100

    add: drone is alive

--- js/game.js
+++ js/game.js
@@ -1,3 +1,42 @@
+drone = {
+    first:true,
+    left0:0,
+    right0:0,
+    direction:"",
+    move:function() {
+        var droneNode = $(".drone");
+
+        var rand = 3;
+        var func, func2;
+        function add(a1,a2) {
+            return a1+a2;
+        }
+        function sub(a1,a2) {
+            return a1-a2;
+        }
+
+        if (drone.direction === "back") {
+            func = sub;
+            func2 = add;
+        } else {
+            func = add;
+            func2 = sub;
+        }
+
+        if (Math.abs(drone.left0 - func(droneNode.position().left,rand)) > 100) {
+            drone.direction = drone.direction === "back" ? "":"back";
+            func = func2;
+        }
+        droneNode.css("left",func(droneNode.position().left,rand));
+    }
+};
+
+function nextPosition() {
+    drone.move();
+
+    setTimeout(nextPosition,40);
+}
+
 function start(root) {
 
     function moveDrone() {
@@ -6,6 +45,8 @@ function start(root) {
         drone.left0 = droneNode.position().left;
         drone.top0 = droneNode.position().top;
         drone.first = false;
+
+        setTimeout(nextPosition,200);
     }
 
     root.css({margin:0});
@@ -13,7 +54,6 @@ function start(root) {
     root.find(".earth").css({position:"fixed", height:"100%"}).attr("src","img/earth.png");
     root.find(".devoxx").css({position:"fixed", height:"100%",opacity:0}).attr("src","img/bg.jpg");
     root.find(".drone").css({position:"fixed", top: 380, left: 120, zIndex:3}).attr("src","img/heli1.png");
-
 
     moveDrone();
 }
