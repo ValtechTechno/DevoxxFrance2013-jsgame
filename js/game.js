dd46b9ab98ef84481dcf1a66edc7f9e287abebcf
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-02-06 17:38:03 +0100

    add: devoxx

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 function start(root) {
     root.css({margin:0});
-    root.append("<img class='earth'> ");
+    root.append("<img class='earth'> ").append("<img class='devoxx'>");
     root.find(".earth").css({position:"fixed", height:"100%"}).attr("src","img/earth.png");
+    root.find(".devoxx").css({position:"fixed", height:"100%",opacity:0}).attr("src","img/bg.jpg");
 }
