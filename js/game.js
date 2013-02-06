ca2927adadc13fd9e061c22454ba1063df23f195
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-02-06 17:39:55 +0100

    add: drone position0

--- js/game.js
+++ js/game.js
@@ -1,7 +1,19 @@
 function start(root) {
+
+    function moveDrone() {
+        var droneNode = root.find(".drone");
+
+        drone.left0 = droneNode.position().left;
+        drone.top0 = droneNode.position().top;
+        drone.first = false;
+    }
+
     root.css({margin:0});
     root.append("<img class='earth'> ").append("<img class='devoxx'>").append("<img class='drone'>");
     root.find(".earth").css({position:"fixed", height:"100%"}).attr("src","img/earth.png");
     root.find(".devoxx").css({position:"fixed", height:"100%",opacity:0}).attr("src","img/bg.jpg");
     root.find(".drone").css({position:"fixed", top: 380, left: 120, zIndex:3}).attr("src","img/heli1.png");
+
+
+    moveDrone();
 }
