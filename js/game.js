b5efc3950f105d27c96a09e32e51b005d3352d02
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-02-08 16:56:39 +0100

    add: move earth on drone launch

--- js/game.js
+++ js/game.js
@@ -5,37 +5,42 @@ drone = {
     left0:0,
     right0:0,
     power:0,
-    direction:"",
-    move:function() {
-        var droneNode = $(".drone");
-
-        var rand = 3;
-        var func, func2;
-        function add(a1,a2) {
-            return a1+a2;
-        }
-        function sub(a1,a2) {
-            return a1-a2;
-        }
-
-        if (drone.direction === "back") {
-            func = sub;
-            func2 = add;
-        } else {
-            func = add;
-            func2 = sub;
-        }
-
-        if (Math.abs(drone.left0 - func(droneNode.position().left,rand)) > 100) {
-            drone.direction = drone.direction === "back" ? "":"back";
-            func = func2;
-        }
-        droneNode.css("left",func(droneNode.position().left,rand));
-    }
+    direction:""
 };
 
+function dronePosition() {
+    function add(a1,a2) {
+        return a1+a2;
+    }
+    function sub(a1,a2) {
+        return a1-a2;
+    }
+
+    var func, func2;
+    if (drone.direction === "back") {
+        func = sub;
+        func2 = add;
+    } else {
+        func = add;
+        func2 = sub;
+    }
+
+    var droneNode = $(".drone");
+    var rand = 3;
+    if (Math.abs(drone.left0 - func(droneNode.position().left,rand)) > 100) {
+        drone.direction = drone.direction === "back" ? "":"back";
+        func = func2;
+    }
+    droneNode.css("left",func(droneNode.position().left,rand));
+}
+
 function nextPosition() {
-    drone.move();
+    if (drone.power > 0) {
+        var $earth = $(".earth");
+        var bottom = parseInt($earth.css("bottom").replace("px",""));
+        $earth.css({bottom:bottom-1});
+    }
+    dronePosition();
 }
 
 function run() {
@@ -56,7 +61,7 @@ function start(root) {
 
     root.css({margin:0});
     root.append("<img class='earth'> ").append("<img class='devoxx'>").append("<img class='drone'>");
-    root.find(".earth").css({position:"fixed", height:"100%"}).attr("src","img/earth.png");
+    root.find(".earth").css({position:"fixed", height:"100%", bottom:0}).attr("src","img/earth.png");
     root.find(".devoxx").css({position:"fixed", height:"100%",opacity:0}).attr("src","img/bg.jpg");
     root.find(".drone").css({position:"fixed", top: 380, left: 120, zIndex:3}).attr("src","img/heli1.png");
 
@@ -66,9 +71,6 @@ function start(root) {
         switch (String.fromCharCode(e.which)) {
             case "I":
                 drone.power = 1;
-                break;
-            default:
-                console.log(String.fromCharCode(e.witch));
                 break;
         }
     });
